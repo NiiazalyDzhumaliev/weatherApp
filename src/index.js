24fa9bb9ce4585f1016986c9bd0ebfcc12cabab3
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-21 21:49:27 +0600

    Add temp to list

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ const city = document.getElementById('city-name');
 const temperature = document.getElementById('weather-temp');
 const form = document.querySelector('.city-form');
 
+
+const toCelsius = inFahr => {
+  const inCels = inFahr - 273.15;
+  const roundDegree = Math.round(inCels);
+  return roundDegree;
+};
+
 async function getWeather(chosenCity) {
   try {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&APPID=9180100ea185ef7a04ff39e412ae0e12`;
-    const response = await fetch(url, {mode: 'cors'});
+    const api = '9180100ea185ef7a04ff39e412ae0e12';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&APPID=${api}`;
+    const response = await fetch(url, { mode: 'cors' });
     const weatherData = await response.json();
-    temperature.textContent = weatherData.main.temp;
+    const currentTemp = toCelsius(weatherData.main.temp);
+    temperature.textContent = `${currentTemp}\xB0C`;    
   } catch (error) {
-    temperature.textContent = 'Wrong city';
+    temperature.textContent = `${chosenCity} is wrong`;
   }
 }
 
