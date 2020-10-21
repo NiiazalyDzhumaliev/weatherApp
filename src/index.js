2a0c7e767dfd87269536681bacc535e36af5139d
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-10-21 21:26:16 +0600

    Fix error in async

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 const city = document.getElementById('city-name');
-const degree = document.getElementById('degree');
+const temperature = document.getElementById('weather-temp');
 const form = document.querySelector('.city-form');
 
 async function getWeather(chosenCity) {
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&APPID=9180100ea185ef7a04ff39e412ae0e12`;
-  const response = await fetch(url, {mode: 'cors'});
-  const weatherData = await response.json();
+  try {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&APPID=9180100ea185ef7a04ff39e412ae0e12`;
+    const response = await fetch(url, {mode: 'cors'});
+    const weatherData = await response.json();
+    temperature.textContent = weatherData.main.temp;
+  } catch (error) {
+    temperature.textContent = 'Wrong city';
+  }
 }
 
 const formSubmit = () => {
@@ -14,7 +19,6 @@ const formSubmit = () => {
     getWeather(chosenCity);
     event.preventDefault();
     form.reset();
-    console.log(chosenCity);
   });
 };
 
